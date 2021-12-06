e63b35ef6ae5d4eb1c433207ef44ac9a03368409
PrivetPediki <95643080+PrivetPediki@users.noreply.github.com>
2021-12-06 22:05:03 +0500

    Create Dick.py

---
+++ Dick.py
@@ -0,0 +1,9 @@
+print('   penis   ')
+print('   penis   ')
+print('   penis   ')
+print('   penis   ')
+print('   penis   ')
+print ('penispenis')
+print ('penispenis')
+
+print('\nsuck some dick please')
